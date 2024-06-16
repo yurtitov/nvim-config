580d2ab59dee0f9d36b0643731fd7209470ae12e
Yuriy Titov <yurtitov@gmail.com>
2024-06-16 11:42:14 +0200

    Backup from script: 2024-06-16 11:42:12

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # nvim configs
 
-## Install
+## Installation
+
+### 1. Make a backup of your current nvim folder
+
+```sh
+mv ~/.config/nvim ~/.config/nvim.bak
+```
+
+### 2. Clean nvim folders (recommended)
+
+```sh
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+```
+
+### 3. Clone the repository
 
 ```sh
 git clone git@github.com:yurtitov/nvim-config.git ~/.config/nvim
+```
+
+### 4. Run nvim
+
+```sh
+nvim
 ```
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -12,7 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ 'phaazon/hop.nvim' },
 	{ 'nvim-neo-tree/neo-tree.nvim',
         branch = "v3.x",
     	dependencies = {
@@ -26,6 +25,9 @@ require("lazy").setup({
 	{ 'neovim/nvim-lspconfig' },
 	{ 'hrsh7th/nvim-cmp' },
 	{ 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' },
 	{ 'saadparwaiz1/cmp_luasnip' },
 	{ 'L3MON4D3/LuaSnip' },
 	{ 'morhetz/gruvbox' }, -- Color scheme
